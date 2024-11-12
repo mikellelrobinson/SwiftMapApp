098b36cc9a001d05a5349fad8e51c6a36902e8ca
mikellerobinson <mikellerobinson@icloud.com>
2024-11-11 20:02:00 -0600

    Initial Commit

---
+++ swiftmapapp/swiftmapappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftmapappApp.swift
+//  swiftmapapp
+//
+//  Created by Rocky on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftmapappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
