45c38c385c25eea5ef64802ecd9efc78ab1d9de0
mikellelrobinson <mikellerobinson+1@icloud.com>
2024-11-11 21:45:26 -0600

    Add MapKit

--- swiftmapapp/Views/LocationsView.swift
+++ swiftmapapp/Views/LocationsView.swift
@@ -6,20 +6,26 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationsView: View {
     
     @EnvironmentObject private var vm: LocationsViewModel
-    
+    @State private var region: MKCoordinateRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
+        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    )
     
     var body: some View {
-        Text("Hello, there!")
+        ZStack {
+            Map() // Correct way to bind the region to the map
+        }
     }
 }
 
 struct LocationsView_Previews: PreviewProvider {
     static var previews: some View {
         LocationsView()
-            .environmentObject(LocationsViewModel())
+            .environmentObject(LocationsViewModel()) // Make sure you have a LocationsViewModel
     }
 }
